21f2bf9e33116d3dd8d03c364aef46fd17ac2a13
Patrick Wu <patrick@spartanradar.com>
2023-02-06 16:08:00 -0800

    Adding Hello World Code

---
+++ build_and_run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Most accurate way to get the directory where script is located
+# Helps with pathing
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd $SCRIPT_DIR
+cmake - S . -B build
+cmake --build build
