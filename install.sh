f3b357dc97e9921961ad9521763ccffa601d7ebc
Patrick Wu <patrick@spartanradar.com>
2023-02-08 13:47:52 -0800

    another fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 wget -O gtest.tar.gz https://github.com/google/googletest/archive/refs/tags/v1.13.0.tar.gz
 mkdir -p gtest && tar xzf gtest.tar.gz -C gtest --strip-components 1
-ls gtest
+cd gtest
 cmake -S . -B gtest/build -DCMAKE_INSTALL_PREFIX=/usr/local
 cmake --build gtest/build -j
 cmake --install gtest/build
