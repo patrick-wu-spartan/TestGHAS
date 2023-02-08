421d35922b70f5a2cbb8c545a06de48bdd4f9c30
Patrick Wu <patrick@spartanradar.com>
2023-02-08 13:08:17 -0800

    Adding more SA warnings that might get caught

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main() {
     // unused variable
     int myUnusedVar;
 
+    int notAFloat = 5.1;
+
+    // out of range indexing
+    int arr[4];
+    for (int i = 0; i < 5; i++) {
+        std::cout << arr[i] << "\n";
+    }
+
     std::cout << "Hello World!";
     return 0;
 }
